0b3d4a73d9e8a38a582634b9c2de75d668ae2f6c
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-20 18:27:30 +0000

    Release 2.0.0-rc.10

--- src/Client.ts
+++ src/Client.ts
@@ -60,8 +60,8 @@ export class PinnacleClient {
                 {
                     "X-Fern-Language": "JavaScript",
                     "X-Fern-SDK-Name": "rcs-js",
-                    "X-Fern-SDK-Version": "2.0.0-rc.11",
-                    "User-Agent": "rcs-js/2.0.0-rc.11",
+                    "X-Fern-SDK-Version": "2.0.0-rc.10",
+                    "User-Agent": "rcs-js/2.0.0-rc.10",
                     "X-Fern-Runtime": core.RUNTIME.type,
                     "X-Fern-Runtime-Version": core.RUNTIME.version,
                 },
